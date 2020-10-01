3830a00a51397f3706190b9cf332179b68917a43
nicoWeb31 <nico.riot@free.fr>
2020-10-01 20:47:46 +0200

    correction

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -11,10 +11,39 @@ function reverse(str) {
     //je split le str en tab et je le reverse  ---split decoupe---reverse change le sens ---- joint concatene les elements du tab 
     const char = str.split('').reverse()
 
-
-    console.log(char)
-    const strRevser = char.join('');
-    return strRevser;
+    return char.join(''); 
 }
+
+
+//v2
+
+// exemple mat
+// function reverse2(str) {
+
+//     let reversed = "";
+
+//     //new synthax js 
+//     for (let char of str){
+    //m ----> a + m ----> t + am 
+//         reversed = char + reversed
+//     }
+
+//     return reversed
+// }
+
+
+
+// //v3 reduce
+// function reverse(str) {
+
+//     //reduce prend une colback
+//     str.split('').reduce((reversed,character)=>{
+//         return character + reversed
+//     },'')
+
+    
+// }
+
+
 
 module.exports = reverse;
