36bbd79fca5065f3fcaa0b29fd4fc8445e48899c
nicoWeb31 <nico.riot@free.fr>
2020-10-05 20:47:11 +0200

    reversInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+
+
+
+//Math.sign renvoi 1 pour un positif ou -1 pour un negatif
+
+function reverseInt(n) {
+    const numberReverse =  parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+    console.log(numberReverse);
+    return numberReverse
+}
 
 module.exports = reverseInt;
