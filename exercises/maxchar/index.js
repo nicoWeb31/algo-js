9dd8b0ceb6117500950b28540dcdf07b03e6d840
nicoWeb31 <nico.riot@free.fr>
2020-10-07 22:20:22 +0200

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    const chars ={}
+    let max = 0;
+    let maxChar = ''
+
+    for(let char of str){
+        !chars[char] ? chars[char] = 1 : chars[char]++
+        
+    }
+
+console.log(chars)
+
+for(let c in chars){
+
+    if(chars[c]>max){
+        max=chars[c]
+        maxChar = c
+    }
+}
+
+return maxChar
+
+}
 
 module.exports = maxChar;
